a28c976bdfbe1904fe4756fbfab6b24dc27cf7fb
BartTheScrivner <dframe22@gmail.com>
2020-07-11 13:49:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+
+
+document.getElementById('main').remove();
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "Dave is the champion";
