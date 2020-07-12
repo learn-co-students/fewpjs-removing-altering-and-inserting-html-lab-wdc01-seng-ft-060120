45c3e9d9430243d82b7a226cd562a5b035f5beef
Diwo777 <diego.zegarra79@icloud.com>
2020-07-12 15:48:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let mein = document.querySelector('main#main')
+mein.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
