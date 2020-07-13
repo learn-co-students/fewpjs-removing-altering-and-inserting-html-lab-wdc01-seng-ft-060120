78d38c589a804044669cb681c8dbe27fdebee98b
DannyCoop <cooperdaniel5141@gmail.com>
2020-07-13 12:24:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Daniel is the champion!";
+newHeader.className = "victory";
